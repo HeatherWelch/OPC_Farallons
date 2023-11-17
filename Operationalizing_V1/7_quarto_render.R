d044dd71b2835319ce50a9316cce825dc0f28851
Heather Welch <heather.welch@noaa.gov>
2023-11-17 12:39:35 -0800

    quarto render

--- Operationalizing_V1/7_quarto_render.R
+++ Operationalizing_V1/7_quarto_render.R
@@ -1,2 +1,15 @@
 library(quarto)
+quarto_path <- function() {
+  path_env <- Sys.getenv("QUARTO_PATH", unset = NA)
+  if (is.na(path_env)) {
+    path <- unname(Sys.which("quarto"))
+    if (nzchar(path)) path else NULL
+  } else {
+    path_env
+  }
+}
+
+path=quarto_path()
+Sys.setenv(QUARTO_PATH=path)
+
 quarto_render("/Users/heatherwelch/Dropbox/OPC_Farallons/operationalization/end_products/README.qmd")
